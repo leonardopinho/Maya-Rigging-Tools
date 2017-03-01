65dcd2e012b8efce792ea57fe9763089a84f1650
Leonardo Pinho <leonardopinho2005@gmail.com>
2017-03-01 20:29:02 -0300

    corretion in path

--- Main.py
+++ Main.py
@@ -7,19 +7,17 @@ try:
     filename = inspect.getframeinfo(inspect.currentframe()).filename
     path = os.path.dirname(os.path.abspath(filename))
 
-    print(path)
+    import src.ui.UI as UI
+
+    reload(UI)
+
+    from src.ui.UI import UI
+
+    if path not in sys.path:
+        sys.path.append(path)
 
 except Exception as e:
     print(e)
-
-if path not in sys.path:
-    sys.path.append(path)
-
-import src.ui.UI as UI
-
-reload(UI)
-
-from src.ui.UI import UI
 
 
 def init():
